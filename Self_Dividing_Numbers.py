f4a2ec718fcc9a9206de4697439d2555961eefc0
samuel samarpana boye <97591586+samuel123457@users.noreply.github.com>
2022-07-02 11:42:00 +0530

    n=int(input()) m=int(input()) for i in range(n,m+1):     l=i     s=i     c=0     f=0     while l!=0:         r=l%10         c+=1         if r==0:             break         if s%r==0:             f+=1         l=l//10     if f==c:         print(i,end=' ')

---
+++ Self_Dividing_Numbers.py
@@ -0,0 +1,17 @@
+n=int(input())
+m=int(input())
+for i in range(n,m+1):
+    l=i
+    s=i
+    c=0
+    f=0
+    while l!=0:
+        r=l%10
+        c+=1
+        if r==0:
+            break
+        if s%r==0:
+            f+=1
+        l=l//10
+    if f==c:
+        print(i,end=' ')
