2ff0d31d601e5c4d65f6f21926f2684ba17ca29a
samuel samarpana boye <97591586+samuel123457@users.noreply.github.com>
2023-08-08 09:56:48 +0530

    A = int(input())
    Condition_1 = A >= (-10 ** 18) and A <= (10 ** 18)
    
    X = A / 10
    Condition_2 = type(X) == type(1.1)
    
    if Condition_1 :
        if Condition_2 :
            ans = (A // 10) 
        else :
            ans = X 
    
    
    
    print(ans)

--- Integer_Division.py
+++ Integer_Division.py
@@ -1,3 +1,16 @@
-a=int(input())
-c=a//10
-print(c)
+A = int(input())
+
+Condition_1 = A >= (-10 ** 18) and A <= (10 ** 18)
+
+X = A / 10
+Condition_2 = type(X) == type(1.1)
+
+if Condition_1 :
+    if Condition_2 :
+        ans = (A // 10) 
+    else :
+        ans = X 
+
+
+
+print(ans)
