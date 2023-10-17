1decc53e3f20019593a548c826d28fea086684f7
samuel samarpana boye <97591586+samuel123457@users.noreply.github.com>
2023-10-17 10:54:28 +0530

    N = int(input()) con = str(N) count = -1 count_1 = 0 for i in str(N):     for j in str(N):         if i == j:             count = count + 1     if count >= 1:         count_1 = count_1 + 1     count = -1 if count_1 > 0:     print("Not Unique Number") else:     print("Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,10 +1,15 @@
-n=input()
-a=[]
-for i in n:
-    if i.isdigit:
-        if i not in a:
-            a.append(i)
-if len(a)==len(n):
-    print("Unique Number")
-else:
+N = int(input())
+con = str(N)
+count = -1
+count_1 = 0 
+for i in str(N):
+    for j in str(N):
+        if i == j:
+            count = count + 1 
+    if count >= 1: 
+        count_1 = count_1 + 1
+    count = -1 
+if count_1 > 0:
     print("Not Unique Number")
+else:
+    print("Unique Number")
